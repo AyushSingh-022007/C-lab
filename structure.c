bb861864399635cc488a9213ea65f2fbfc4aa70a
AyushSingh-022007 <suir8780@gmail.com>
2025-10-13 18:52:05 +0530

    roll

---
+++ structure.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <String.h>
+struct student{
+    int rollno;
+    char name[100];
+}s1;
+void Display(struct student);
+void Display(struct student s1)
+{
+    printf("Name is %s:",s1.name);
+    printf("Roll no is %d:",s1.rollno);
+}
+int main()
+{
+    s1.rollno=12;
+    strcpy(s1.name,"Ayush");
+    Display(s1);
+}
