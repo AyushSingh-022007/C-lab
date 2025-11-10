7d35472b77fcced8d56bd7f4f39e50b7b4a65700
AyushSingh-022007 <suir8780@gmail.com>
2025-11-10 14:30:00 +0530

    Lab

---
+++ file2.c
@@ -0,0 +1,22 @@
+//Open an existing file and read its content character by character, and then close the file.
+#include<stdio.h>
+int main()
+{
+    FILE *ptr;
+    char ch;
+    ptr=fopen("labtext2.txt","r");
+    if(ptr==NULL)
+    {
+        printf(" Not exists");
+    }
+    else
+    {
+        while((ch=fgetc(ptr))!=EOF)
+        {
+           printf("%c",ch);
+           
+        }
+    } 
+    fclose(ptr);
+    return 0;
+}
